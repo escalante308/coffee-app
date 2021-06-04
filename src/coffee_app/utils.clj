c6824b03c3a101650f90ed57802fa36e2cd2e3b5
Pedro Escalante <escalante308@gmail.com>
2021-06-04 08:57:16 -0500

    First Commit

---
+++ src/coffee_app/utils.clj
@@ -0,0 +1,10 @@
+(ns coffee-app.utils)
+
+(defn calculate-coffee-price [coffees coffee-type number]
+  (->
+   (get coffees coffee-type)
+   (* number)
+   float))
+
+(defn display-bought-coffee-message [type number total]
+  (println "Buying" number (name type) "coffees for total:€" total))
